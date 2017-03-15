a0043634cf91700605afeb1bfff4570e99f84df3
Elen <elencontreras6@hotmail.com>
2017-03-14 21:18:23 -0500

    Aplicacion terminada

--- app/src/main/java/com/example/soporte/holamundo/Principal.java
+++ app/src/main/java/com/example/soporte/holamundo/Principal.java
@@ -1,33 +1,72 @@
 package com.example.soporte.holamundo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
-
+    //Declaración de variables
     private EditText cajaNombre;
-    private TextView cajaMensaje;
+    private TextView cajaApellido;
+    private Intent i; //Objeto que permite pasar de una actividad a otra.
+    private Bundle b; //Objeto para capsular los datos- para pasar la información de una actividad a otra.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
+        //Capturar cajas
         cajaNombre = (EditText)findViewById(R.id.txtNombre);
+        cajaApellido=(TextView)findViewById(R.id.txtApellido);
 
-        //cajaMensaje = (TextView)findViewById(R.id.lblMensaje);
+        //Crear Intent
+        i= new Intent(this,Saludo.class);
+
+        //Crear capsula
+        b = new Bundle();
     }
 
 
     public void saludar(View v){
-        String aux;
-        aux = cajaNombre.getText().toString();
-      //  cajaMensaje.setText(getResources().getString(R.string.parte_saludo)+" "+aux);
-     //   cajaMensaje.setText(getResources().getString(R.));
+
+        String nomb, apell;
+
+
+
+        //Capturar valores de las cajas
+        nomb = cajaNombre.getText().toString();
+        apell= cajaApellido.getText().toString();
+
+        //Validar
+        if (validar()) {
+
+            b.putString("Nombre", nomb); //Encapsular los valores previamente tomados
+            b.putString("Apellido", apell);
+
+            i.putExtras(b); // Pasar al Intent los datos en forma encapsulada por el bundle
+
+            startActivity(i); //Arrancar actividad del Intent
+        }
+    }
+
+    public boolean validar(){
+        if (cajaNombre.getText().toString().isEmpty()){
+           // Toast.makeText(getApplicationContext(),"Digite por favor el nombre",Toast.LENGTH_SHORT).show();
+            cajaNombre.setError(getResources().getString(R.string.error_1));
+            return false;
+        }
+        if (cajaApellido.getText().toString().isEmpty()){
+            //Toast.makeText(getApplicationContext(),"Digite por favor el apellido",Toast.LENGTH_SHORT).show();
+            cajaApellido.setError(getResources().getString(R.string.error_2));
+
+            return false;
+        }
+        return true;
     }
 }
 
--- app/src/main/java/com/example/soporte/holamundo/Saludo.java
+++ app/src/main/java/com/example/soporte/holamundo/Saludo.java
@@ -1,13 +1,40 @@
 package com.example.soporte.holamundo;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Saludo extends AppCompatActivity {
+
+    private TextView saludo;
+    private Bundle b;
+    private String aux, nomb, apell;
+    private Resources res; //Objeto para acceder a los recursos
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo);
+
+        saludo = (TextView)findViewById(R.id.txtSaludo);
+
+        //Capturar capsula
+        b = getIntent().getExtras();
+
+        //Obtener los datos de la capsula
+        nomb = b.getString("Nombre");
+        apell = b.getString("Apellido");
+
+        // aux = "Hola "+nomb+" "+apell+" ¡Bienvenido a esta aplicación!";
+
+        //Acceder a los recursos
+        res =this.getResources();
+
+        //Para que pueda ser en ingles
+        aux= res.getString(R.string.parte_saludo)+" "+nomb+" "+apell+" "+res.getString(R.string.parte_saludo2);
+
+        saludo.setText(aux);
+
     }
 }
